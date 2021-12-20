01ca1ee7d24f9c3676102a2d24b116f1a911ec19
liushouping <kose146352@gmail.ocm>
2021-12-20 18:01:59 +0800

    14個網域彩蛋文字預告

---
+++ components/Tutorial.vue
@@ -0,0 +1,21 @@
+<!-- Please remove this file from your project -->
+<template>
+  <div
+    class="flex justify-center items-start tracking-wider bg-black h-screen w-screen text-white flex-col px-10 md:px-20">
+        <div
+        class="text-2xl md:text-4xl">
+            <span
+            class="text-5xl">WTF</span> are you here, wondering around for?
+        </div>
+        <p
+        class="mt-20 md:mt-0 text-2xl md:text-4xl">Oh looking for secrets huh?</p>
+        <p
+        class="mt-10 md:mt-20 text-2xl md:text-4xl">We're not telling and it's probably nothing...</p>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'NuxtTutorial'
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,17 @@
+<template>
+  <Tutorial/>
+</template>
+
+<script>
+export default {
+  name: 'IndexPage'
+}
+</script>
+
+<style>
+  *{
+    font-family: cooper-black-std,serif;
+    font-weight: 400;
+    font-style: normal;
+  }
+</style>
